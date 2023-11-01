c5c054f1c921a9610b962054e5dcd7c16f9b717f
James0711 <jengreat94@gmail.com>
2023-11-01 01:36:20 +0000

    AirBnB clone - RESTful API

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -44,7 +44,7 @@ class FileStorage:
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
         for key in self.__objects:
-            json_objects[key] = self.__objects[key].to_dict()
+            json_objects[key] = self.__objects[key].to_dict(True)
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
@@ -68,3 +68,28 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """ returns a given object based on cls and id """
+
+        if type(cls) is str:
+            cls = eval(cls)
+        objs = self.all(cls)
+
+        for obj in objs.values():
+            if obj.id == id:
+                return obj
+
+        return None
+
+    def count(self, cls=None):
+        """ returns the number of objects in storage """
+
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+            objs = self.all(cls)
+        else:
+            objs = self.all()
+
+        return(len(objs))
